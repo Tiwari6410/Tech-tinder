3ba73327117c0c1c502f4ea830097612ae36d3d9
demomo <your.name@domain.com>
2025-05-02 18:11:58 +0530

    create express server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hy hello")
+const express = require("express")
+const app = express();
+
+app.use("/", (req, res) => {
+    res.send("Hy i am from home page ")
+})
+// app.get("/test", (req, res) => {
+//     res.send("Hy i am from Test page ")
+// })
+
+app.listen(7000, () => {
+    console.log("Server is successfully running")
+})
