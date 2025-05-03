2b20cbb74f499916505e459776319a4eee33c29d
demomo <your.name@domain.com>
2025-05-03 15:05:23 +0530

    played with routes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const app = express();
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hy i am from home page ")
 })
-// app.get("/test", (req, res) => {
-//     res.send("Hy i am from Test page ")
-// })
+app.use("/test", (req, res) => {
+    res.send("Hy i am from Test page ")
+})
+app.use("/contact", (req, res) => {
+    res.send("Hy i am from contact page ")
+})
 
 app.listen(7000, () => {
     console.log("Server is successfully running")
--- homework.md
+++ homework.md
@@ -15,3 +15,6 @@
 - Push all code to remote origin
 - Play with routes and route extensions ex. /hello, /, /hello/2, /xyz
 - Order of the routes matter a lot
+
+- Install Postman app and make a workspace/collection > test API Call
+- Write logic to handle GET, POST, PATCH, DELETE API Calls and test them on Postman
